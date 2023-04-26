71d2fa165b97bb2498e7b346516cc6d459090458
MageCoven <casper.lindberg.github@gmail.com>
2023-04-26 19:22:50 +0200

    test!: created tests for add function

--- src/index.test.ts
+++ src/index.test.ts
@@ -25,7 +25,11 @@ describe("Vector2", () => {
                 [typeof {}, {}],
             ])("Such as %ss", (type, value) => {
                 // @ts-expect-error explicitly testing for type violations
-                expect(new Vector2(value, value)).toThrow();
+                expect(() => new Vector2(value, value)).toThrow();
+                // @ts-expect-error explicitly testing for type violations
+                expect(() => new Vector2(0, value)).toThrow();
+                // @ts-expect-error explicitly testing for type violations
+                expect(() => new Vector2(value, 0)).toThrow();
             });
         });
         describe("That are numbers should not throw an error", () => {
@@ -34,8 +38,61 @@ describe("Vector2", () => {
                 [typeof BigInt(0), BigInt(0)],
             ])("Such as %ss", (type, value) => {
                 // @ts-expect-error explicitly testing for type violations
-                expect(new Vector2(value, value)).not.toThrow();
+                expect(() => new Vector2(value, value)).not.toThrow();
             });
         });
+    });
+
+    describe("Adding vector A and B", () => {
+        test("Vector2.add() passed a Vector2 does not throw an error", () => {
+            expect(() =>
+                new Vector2(0, 0).add(new Vector2(0, 0)),
+            ).not.toThrow();
+        });
+
+        test("Vector2.add() passed a non Vector2 throws an error", () => {
+            // @ts-expect-error explicitly testing for type violations
+            expect(() => new Vector2(0, 0).add("")).toThrow();
+        });
+
+        test("Returns a Vector2", () => {
+            const A = new Vector2(0, 0);
+            const B = new Vector2(1, 1);
+            expect(A.add(B)).toBeInstanceOf(Vector2);
+        });
+
+        test("Returns a new Vector2", () => {
+            const A = new Vector2(0, 0);
+            const B = new Vector2(1, 1);
+            expect(A.add(B)).not.toBe(A);
+            expect(A.add(B)).not.toBe(B);
+        });
+
+        test.each([
+            [0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1],
+            [0, 0, 1, 0, 1, 0],
+            [0, 0, 0, 1, 0, 1],
+            [-1, 0, 0, 0, -1, 0],
+            [0, -1, 0, 0, 0, -1],
+            [0, 0, -1, 0, -1, 0],
+            [0, 0, 0, -1, 0, -1],
+            [1, 0, 1, 0, 2, 0],
+            [0, 1, 0, 1, 0, 2],
+            [1, 0, -1, 0, 0, 0],
+            [0, 1, 0, -1, 0, 0],
+            [-1, 0, 1, 0, 0, 0],
+            [0, -1, 0, 1, 0, 0],
+        ])(
+            "Where A = (%d, %d) and B = (%d, %d) should equal (%d, %d)",
+            (a, b, c, d, e, f) => {
+                const A = new Vector2(a, b);
+                const B = new Vector2(c, d);
+                const C = new Vector2(e, f);
+                expect(A.add(B).x).toBe(C.x);
+                expect(A.add(B).y).toBe(C.y);
+            },
+        );
     });
 });
