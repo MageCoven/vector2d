7209a0f8f7a8d87c921781fe185346ff8adf63d3
MageCoven <casper.lindberg.github@gmail.com>
2023-04-26 19:22:19 +0200

    test!: made tests more clear and complete

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,41 @@
 import { Vector2 } from ".";
 
 describe("Vector2", () => {
-    test("Create Vector2", () => {
-        expect(Vector2).toBeDefined();
+    describe("Has correctly initialized property", () => {
+        test.each([["x"], ["y"]])("%s", (property) => {
+            expect(new Vector2(0, 0)).toHaveProperty(property, 0);
+        });
+    });
 
-        const vector = new Vector2(0, 1);
-        expect(vector).toBeDefined();
-        expect(vector.x).toBe(0);
-        expect(vector.y).toBe(1);
+    describe("Created with arguments", () => {
+        describe("That are not numbers should throw an error", () => {
+            test.each([
+                [typeof "0", "0"],
+                [
+                    typeof (() => {
+                        /**/
+                    }),
+                    () => {
+                        /**/
+                    },
+                ],
+                [typeof undefined, undefined],
+                [typeof true, true],
+                [typeof Symbol("symbol"), Symbol("symbol")],
+                [typeof {}, {}],
+            ])("Such as %ss", (type, value) => {
+                // @ts-expect-error explicitly testing for type violations
+                expect(new Vector2(value, value)).toThrow();
+            });
+        });
+        describe("That are numbers should not throw an error", () => {
+            test.each([
+                [typeof 0, 0],
+                [typeof BigInt(0), BigInt(0)],
+            ])("Such as %ss", (type, value) => {
+                // @ts-expect-error explicitly testing for type violations
+                expect(new Vector2(value, value)).not.toThrow();
+            });
+        });
     });
 });
