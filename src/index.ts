2f3d8547883e82c42121a3a4bbfb6fbd1f7ec67e
MageCoven <casper.lindberg.github@gmail.com>
2023-04-26 19:33:49 +0200

    feat: implemented add function

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,24 @@ export class Vector2 {
      * @param y The y component
      */
     constructor(x: number, y: number) {
+        if (typeof x !== "number" && typeof x !== "bigint")
+            throw new TypeError(
+                `Expected first argument of Vector2 to be a number or BigInt but got "${typeof x}"`,
+            );
+
+        if (typeof y !== "number" && typeof y !== "bigint")
+            throw new TypeError(
+                `Expected second argument of Vector2 to be a number or BigInt but got "${typeof y}"`,
+            );
+
         this.x = x;
         this.y = y;
+    }
+
+    add(other: Vector2): Vector2 {
+        if (!(other instanceof Vector2))
+            throw TypeError("Can only add with a Vector2");
+
+        return new Vector2(this.x + other.x, this.y + other.y);
     }
 }
